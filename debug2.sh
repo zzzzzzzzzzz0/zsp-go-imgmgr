1fbe7eaed07ceb1ccebbfe19989a244a07bf433f
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2015-02-13 10:22:04 +0800

    修改

--- debug2.sh
+++ debug2.sh
@@ -2,9 +2,4 @@
 cd `dirname $0`/root
 ../bin/zsp \
 -zhscript-o-ansi \
--zhscript-o-args \
--zhscript-o-tree \
--zhscript-o-path \
--zhscript-o-lc \
--zhscript-o-lc2 \
 ---- -r $PWD/ -a :4001 "$@"
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-d=`dirname $0`
-cd $d/root
+cd `dirname $0`/root
 port=4001
 xdg-open "http://127.0.0.1:$port"
 ../bin/zsp ---- -r $PWD/ -a :$port "$@"
