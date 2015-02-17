eb76f82d373b2437c1530114aa2f6aecc7917558
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2015-02-17 17:32:44 +0800

    新春到

--- root/1.js
+++ root/1.js
@@ -7,8 +7,20 @@ function e_tn__(s) {
 function e_cn__(s) {
 	return document.getElementsByClassName(s);
 }
+function e_n__(s) {
+	return document.getElementsByName(s);
+}
 function o_e__(s) {
 	return opener.document.getElementById(s);
+}
+
+function focus__(s) {
+	alert(s + " 哈");
+	
+	//window.focus();
+
+	/*setTimeout(window.focus, 300);
+	window.blur();*/
 }
 
 function exec__(s, f) {
