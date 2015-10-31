abd9b3ecfc2ebda4f70c370903d737b63f7163c9
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2015-10-31 13:31:09 +0800

    改

--- root/zhugeshow.js
+++ root/zhugeshow.js
@@ -3,11 +3,24 @@ function zhugeshow__() {
 	var imgs = document.getElementsByTagName("img");
 	for(var i = 0; i < imgs.length; i++) {
 		var img = imgs[i];
-		var y = img.offsetTop;
-		if(y + img.height >= y1 && y <= y2) {
+		if(img.attributes["src2o"])
+			continue;
+		var y = img.offsetTop, h = img.height;
+		var h_1 = h, h_2 = 0;
+		if(!h) {
+			var src2h = img.attributes["src2h"];
+			if(src2h) {
+				h = parseInt(src2h.nodeValue);
+				h_1 = h_2 = h / 2;
+			}
+		}
+		if(y + h_1 >= y1 && y - h_2 <= y2) {
 			var src2 = img.attributes["src2"];
-			if(src2)
+			if(src2) {
 				img.src = src2.nodeValue;
+				img.setAttribute("src2o", "");
+				//console.log(i+") "+h_1+" "+h_2);
+			}
 		}
 	}
 }
